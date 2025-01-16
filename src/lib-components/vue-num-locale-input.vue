177ccb54076c5c64ad28e6ea6528ffb578ed634c
Radek Svítil <radek.svitil@geology.cz>
2025-01-16 16:25:58 +0100

    Updated code by chatgpt advice

--- src/lib-components/vue-num-locale-input.vue
+++ src/lib-components/vue-num-locale-input.vue
@@ -1,29 +1,24 @@
 <template>
   <input
+    v-if="shown === 'text'"
     type="text"
-    v-bind="$attrs"
-    :style="shown === 'number' ? 'display: none' : ''"
+    v-bind="filteredAttrs"
     readonly
-    :value="
-      typeof modelValue === 'number'
-        ? modelValue.toLocaleString(undefined, options)
-        : ''
-    "
+    :value="formattedValue"
     @focus="handleNumberFocusIn"
   />
   <input
+    v-else
     type="number"
-    v-bind="$attrs"
+    v-bind="filteredAttrs"
     v-model="internalValue"
-    @input="updateValue"
-    :style="shown === 'text' ? 'display: none' : ''"
     @focusout="handleNumberFocusOut"
     ref="inpnum"
   />
 </template>
 
 <script setup lang="ts">
-import { ref, nextTick } from "vue";
+import { ref, nextTick, computed, useAttrs } from "vue";
 
 const props = defineProps({
   modelValue: {
@@ -40,33 +35,34 @@ const props = defineProps({
 
 const emit = defineEmits(["update:modelValue"]);
 
-const internalValue = ref<number>();
-
-const updateValue = () => {
-  /* if (isNaN(parseFloat(event.target.value)) === false) {
-    emit("update:modelValue", Number(event.target.value));
-  } */
-  if (
-    internalValue.value !== undefined &&
-    internalValue.value !== null &&
-    isNaN(internalValue.value) === false
-  ) {
-    emit("update:modelValue", internalValue.value);
-  }
-};
-
-/** HTML Input type=number element */
-const inpnum = ref<HTMLElement | null>(null);
-
 /** Which type of input is shown? */
 const shown = ref("text");
 
+/** HTML Input type=number element */
+const inpnum = ref<HTMLInputElement | null>(null);
+
+/** Number formatted as locale string */
+const formattedValue = computed(() => {
+  return typeof props.modelValue === 'number'
+    ? props.modelValue.toLocaleString(undefined, props.options)
+    : '';
+});
+
+/** Internal value for input type=number */
+const internalValue = computed({
+  get: () => props.modelValue,
+  set: (val) => {
+    if (val !== undefined && val !== null && !isNaN(val)) {
+      emit("update:modelValue", val);
+    }
+  },
+});
+
 /** Handle FocusIn event on input type=text - replace text input with number input */
 const handleNumberFocusIn = async () => {
-  internalValue.value = props.modelValue;
   shown.value = "number";
-  if (inpnum.value !== null) {
-    await nextTick();
+  await nextTick();
+  if (inpnum.value instanceof HTMLInputElement) {
     inpnum.value.focus();
   }
 };
@@ -75,4 +71,11 @@ const handleNumberFocusIn = async () => {
 const handleNumberFocusOut = () => {
   shown.value = "text";
 };
+
+/** Prevent passing unwanted attributes to input elements */
+const attrs = useAttrs();
+const filteredAttrs = computed(() => {
+  const { type, value, ...rest } = attrs;
+  return rest;
+});
 </script>
